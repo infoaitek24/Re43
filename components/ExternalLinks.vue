4ee41e7f0c2f22962242f2369f6723104b4ec591
infoaitek24 <161408874+infoaitek24@users.noreply.github.com>
2024-03-30 22:10:47 +0800

    Initialize patch-1

---
+++ components/ExternalLinks.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <iframe v-if="links.imdb_id"
+            :src="`https://vidsrc.xyz/embed/movie/${links.imdb_id}`"
+            referrerpolicy="origin"
+            width="640"
+            height="480"
+            allowfullscreen>
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+iframe {
+  width: 100%; /* Responsive width */
+  height: 500px; /* Fixed height, can adjust as needed */
+  border: none; /* Remove default border */
+  border-radius: 8px; /* Optional: rounded corners for the iframe */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Optional: add shadow for better visual separation */
+}
+</style>
---
+++ components/global/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer :class="$style.footer">
+    <p>&copy; {{ new Date().getFullYear() }}  <a target="_blank" href="https://jason.codes/cookie-policy" rel="noopener">All rights reserved Momuflix Media</a></p>
+    <p> <a target="_blank" href="https://www.themoviedb.org/" rel="noopener"></a></p>
+  </footer>
+</template>
+
+<style lang="scss" module>
+@import '~/assets/css/utilities/_variables.scss';
+
+.footer {
+  padding: 2rem 1.5rem 6.5rem;
+  color: $text-color-grey;
+
+  @media (min-width: $breakpoint-small) {
+    padding-right: 4rem;
+    padding-left: 4rem;
+  }
+
+  @media (min-width: $breakpoint-large) {
+    padding: 2rem 5rem;
+  }
+
+  p {
+    margin: 0.3rem 0;
+    font-size: 1.4rem;
+  }
+
+  a {
+    color: $text-color-grey;
+    text-decoration: underline;
+  }
+}
+</style>
+
